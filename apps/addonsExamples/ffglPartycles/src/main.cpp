de5d0f8ef40520e468c108b8e3f6b4e7e23553e3
drand48 <drand48@36fa6012-2abf-11df-9ab6-b1cdc0d4a89a>
2010-12-08 15:04:50 +0000

    changed name

--- apps/addonsExamples/ffglPartycles/src/main.cpp
+++ apps/addonsExamples/ffglPartycles/src/main.cpp
@@ -28,14 +28,14 @@ DEFINE_FFGL_PLUGIN( App, 0, 0 );
 static CFFGLPluginInfo PluginInfo (
 	createPluginInstance,					// * Create method
 	"OFPX",									// Plugin unique ID
-	"Partycles",								// Plugin name
+	"FireWorks",								// Plugin name
 	1,										// * API major version number
 	500,									// * API minor version number
 	1,										// Plugin major version number
 	000,									// Plugin minor version number
 	FF_SOURCE,								// Plugin type FF_SOURCE or FF_EFFECT ( filters ) 
 	"Firework Particles Plugin (Made with OpenFrameworks)",			// Plugin description
-	"by Daniel Berio - www.enist.org"		// About
+	"by Daniel Berio for Resolume"		// About
 	,0
 	,NULL
 );
